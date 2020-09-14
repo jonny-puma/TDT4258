024d90ac812db3b905567f71e0145a678884ae1d
Johannes Giercksky Nilssen <johanngn@stud.ntnu.no>
2020-09-14 14:18:20 +0200

    makefile targets from top directory

--- Exercise1/efficient/ex1.s
+++ Exercise1/efficient/ex1.s
@@ -4,8 +4,8 @@
 
 	/////////////////////////////////////////////////////////////////////////////
 	//
-  // Exception vector table
-  // This table contains addresses for all exception handlers
+  	// Exception vector table
+  	// This table contains addresses for all exception handlers
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
@@ -84,7 +84,7 @@
 _reset: 
 	    ///CMU enable GPIO///
 		mov r0, #1
-		ldr r1, = CMU_BASE
+		ldr r1, =CMU_BASE
 		ldr r2, [r1, #CMU_HFPERCLKEN0]
 		lsl r0, r0, #CMU_HFPERCLKEN0_GPIO
 		orr r2, r2, r0
@@ -92,21 +92,23 @@ _reset:
 
 		//// LEDS ////
 		ldr r1, =GPIO_PA_BASE
-		mov r2, #0x2
+		mov r2, #0x2 // Find power for led
 		str r2, [r1, #GPIO_CTRL]
 
 		ldr r2, =0x55555555
 		str r2, [r1, #GPIO_MODEH]
 		
-		ldr r0, =0xff
+		// Turn leds off
+		mov r0, #0xff
 		lsl r0, r0, #8
 		str r0, [r1, #GPIO_DOUT]
 
 		ldr r3, =GPIO_PC_BASE
 		ldr r2, =0x33333333
 		str r2, [r3, #GPIO_MODEL]
-		ldr r2, =0xff
+		mov r2, #0xff
 		str r2, [r3, #GPIO_DOUT]
+<<<<<<< HEAD
         
         // enable GPIO interrupts
         ldr r0, =0x22222222
@@ -132,6 +134,35 @@ _reset:
 main:
 		
 	    b .
+=======
+
+		// Enabling interrupts
+		ldr r3, =GPIO_BASE
+		ldr r2, =0x22222222
+		str r2, [r3, #GPIO_EXTIPSELL]
+
+		mov r2, #0xff
+		str r2, [r3, #GPIO_EXTIFALL]
+		str r2, [r3, #GPIO_EXTIRISE]
+		str r2, [r3, #GPIO_IEN]
+
+		ldr r2, =0x802
+		ldr r1, =ISER0
+		str r2, [r1]
+
+		ldr r1, =GPIO_PA_BASE
+
+		// Power saving in general here.
+		ldr r2, =SCR
+        mov r0, #0x6
+        str r0, [r2]
+
+        ldr r2, =GPIO_PC_BASE
+		wfi
+		
+
+
+>>>>>>> a210c5d3257033c96a39cd2fc72e379dbddea9ec
 	
 	/////////////////////////////////////////////////////////////////////////////
 	//
@@ -140,8 +171,13 @@ main:
 	//
 	/////////////////////////////////////////////////////////////////////////////
 	
+// r0 multivariate
+// r1 GPIO_PA_BASE
+// r2 GPIO_PC_BASE
+// r3 GPIO BASE
         .thumb_func
 gpio_handler:
+<<<<<<< HEAD
         // clear interupt
         ldr r0, =GPIO_BASE
         ldr r1, [r0, #GPIO_IF]
@@ -159,6 +195,20 @@ gpio_handler:
         // return to main
         b main
 	
+=======
+		// Read and reset interrupt
+		//ldr r1, =GPIO_PA_BASE
+		ldr r0, [r3, #GPIO_IF]
+		str r0, [r3, #GPIO_IFC]
+
+		ldr r0, [r2, #GPIO_DIN]
+        lsl r0, r0, #8
+        str r0, [r1, #GPIO_DOUT]
+		
+		wfi
+	    bx lr
+
+>>>>>>> a210c5d3257033c96a39cd2fc72e379dbddea9ec
 	/////////////////////////////////////////////////////////////////////////////
 	
         .thumb_func
