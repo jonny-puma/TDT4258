05dccb07e71dd514095b220a28164db7e9a78721
speedup <speedup@NTNU18481.win.ntnu.no>
2020-10-01 15:33:09 +0200

    started on shell for playWaveform

--- Exercise2/Baseline/dac.c
+++ Exercise2/Baseline/dac.c
@@ -2,16 +2,20 @@
 #include <stdbool.h>
 
 #include "efm32gg.h"
+#include "common.h"
 
 void setupDAC()
 {
 	/*
 	 * TODO enable and set up the Digital-Analog Converter
 	 * 
-	 * 1. Enable the DAC clock by setting bit 17 in CMU_HFPERCLKEN0 2.
-	 * Prescale DAC clock by writing 0x50010 to DAC0_CTRL 3. Enable left
+	 * 1. Enable the DAC clock by setting bit 17 in CMU_HFPERCLKEN0 
+	 * 2.
+	 * Prescale DAC clock by writing 0x50010 to DAC0_CTRL 
+	 * 3. Enable left
 	 * and right audio channels by writing 1 to DAC0_CH0CTRL and
-	 * DAC0_CH1CTRL 4. Write a continuous stream of samples to the DAC
+	 * DAC0_CH1CTRL 
+	 * 4. Write a continuous stream of samples to the DAC
 	 * data registers, DAC0_CH0DATA and DAC0_CH1DATA, for example from a
 	 * timer interrupt 
 	 */
@@ -23,8 +27,28 @@ void setupDAC()
 
 void playRaw(){
 	// Something something
+	// Generate C array or something
 }
 
-void playWaveform(){
+void playWaveform(int type){
 	// Something else something else
+	// Generate waveform
+	switch( type )
+	{
+		case SINE:
+				
+				Break;
+		case SAWTOOTH:
+				
+				Break;
+		case TRIANGLE:
+				
+				Break;
+		case TRIANGLE:
+				
+				Break;
+		default:
+				// Something went wrong
+				Break;
+	}
 }
--- Exercise2/Baseline/ex2.c
+++ Exercise2/Baseline/ex2.c
@@ -14,6 +14,10 @@
  */
  //TODO: Find this value
 #define   SAMPLE_PERIOD   0
+#define   SINE 0
+#define   SAWTOOTH 1
+#define   TRIANGLE 2
+#define   SQUARE 3
 
 /*
  * Declaration of peripheral setup functions 
