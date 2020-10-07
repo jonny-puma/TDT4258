405a1fddf1eb3596e55bee6767396fa91317eadd
akselgundersen <aksel.gundersen@hotmail.com>
2020-10-07 12:58:20 +0200

    flaaklypa added

--- Exercise2/Baseline/music.h
+++ Exercise2/Baseline/music.h
@@ -1,21 +1,20 @@
 #include <stdint.h>
 
 // Frequency for given notes in Hz. Number indicates octave.
-#define B6 1975
-#define E7 2637
-#define A5 880
-#define B5 987
-#define C5 523
-#define F5 698
-#define G5 783
-#define E5 659
-#define B4 493
-#define C4 261
-#define D4 293
-#define A3 220
-#define dA3 207
-#define A2 110
-#define dA2 103
+#define B6 1975*2
+#define E7 2637*2
+#define A5 880*2
+#define B5 987*2
+#define C5 523*2
+#define F5 698*2
+#define G5 783*2
+#define E5 659*2
+#define B4 493*2
+#define C4 261*4
+#define D4 293*4
+#define A3 220*4
+#define dA3 207*8
+#define dA2 103*8
 //-----------------------------------------------------------
 
 typedef struct 
