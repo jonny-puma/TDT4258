af57440b6fb788ee0ebf1c855f2a04bd77aaf853
Jonas Sundseth <jonassundseth@dhcp-10-22-64-180.wlan.ntnu.no>
2020-11-05 12:05:35 +0100

    Aksel er noob

--- Exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
+++ Exercise3/OSELAS.BSP-EnergyMicro-Gecko/local_src/game-1.0/game.h
@@ -22,10 +22,10 @@
 #define OB_W 10
 
 #define BIRD_COLOR 0x3333
-#define BIRD_H 8
-#define BIRD_W 8
+#define BIRD_H 12
+#define BIRD_W 17
 
-#define OB_GAP 40
+#define OB_GAP 75
 #define OB_COLOR 0x1414
 
 #define BACKGROUND_COLOR 0xffff
